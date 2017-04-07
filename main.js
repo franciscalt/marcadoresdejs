70cc5eeaedd9bdf41bafe4491b316751d0ce248a
francisca Lathrop <franciscalathrop@MacBook-Air-de-Fran.local>
2017-04-06 21:19:49 -0300

    repaso

---
+++ main.js
@@ -0,0 +1,40 @@
+// //Loops
+// // //for
+    
+// // for (var i=0; i < 10; i++){
+// // console.log(i)
+// // }
+
+// // var i = 0
+// // while(i<10) {
+// // if(i<5){ 
+// //     console.log(i + 'es menor que 5 ')
+// // }else{  
+// //      console.log(i + 'es meyor de 5 ')
+// // }
+// // i++
+// // }
+
+
+// //arreglos son para 
+
+
+// var vie = 'viernes'
+// var sab = 'sabado'
+// var dm = 'domingo'
+// var diasdelasemana = ['lunes', 'martes', 'miercoles', ' jueves', vie, sab, dm]
+// console.log(diasdelasemana)
+
+// //todos los arreglos vienen con una propiedad de la que podemos ver la longitus de arreglo 
+
+// console.log(diasdelasemana.length)
+
+// for (var i=0; i < diasdelasemana.length; i++){
+// console.log(diasdelasemana[i])
+// }
+
+// //**ejercicio
+/*escribir una arreglo que contenga todos los meses del años
+/+pedirele al usuario que ingrese el mes de su cumpleaños y se tiene que immprimir todos os mese qu su no cumpleaños se debe utilizar for e if 
+
+ */
